d541443c49ba81174f1be07321ee8373fd5110a9
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-03 12:29:01 -0500

    prop and clickHandlers

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import Greet from './Greet';
 import Counter from './Counter';
 
-function App() {
-  return (
-    <div>
-      <Counter 
-        changeBy={2}
-        initialValue={10}/>
-      <Counter 
-        changeBy={3}
-        initialValue={99}/>
-      <Counter 
+
+
+// function App() {
+//   return (
+//     <div>
+//       <Counter 
+//         changeBy={2}
+//         initialValue={10}/>
+//       <Counter 
+//         changeBy={3}
+//         initialValue={99}/>
+      {/* <Counter 
         changeBy={-4}
         initialValue={5}/>
       <Greet
@@ -21,9 +23,49 @@ function App() {
       how="Salutations" />   
       <Greet
       whom="Chris"
-      how="Howdy" /> 
-    </div>
-  );
-}
+      how="Howdy" />  */}
+//     </div>
+//   );
+// }
 
+// export default App;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: [
+        100,
+        990
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Counter 
+          changeBy={2}
+          value={this.state.values[0]}
+          clickHandler={this._updateValue}
+          index={0}
+          />
+        <Counter 
+          changeBy={3}
+          value={this.state.values[1]}
+          clickHandler={this._updateValue}
+          index={1}
+          />
+        </div>
+    );
+  }
+  _updateValue = (index, newValue) =>  {
+    const newValues = [
+      ...this.state.values
+    ];
+    newValues[index] = newValue;
+    this.setState({
+      values: newValues
+    });
+  }
+}
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -6,37 +6,43 @@ class Counter extends React.Component {
         super(props);
         // Use react.component's constructor to set up Counter to work with React
         this.state={
-            changeBy: props.changeBy,
-            value: props.initialValue
+        //     changeBy: props.changeBy,
+        //       value: props.initialValue
+            initialValue: props.value
         };
     }
 
     render() {
         return(
             <div>
-                <h1>{this.state.value}</h1>
+                <h1>{this.props.value}</h1>
                 <button  onClick={this._incrementValue}>+</button>
                 <button  onClick={this._subValue}>-</button>
-                <button  onClick={this._setValue}>*</button>
+                {/* <button  onClick={this._setValue}>*</button> */}
             </div>
         );
     }
     
 // write helper function as arrow func. they retain the correct value of the THIS keyword
     _incrementValue= () => {
-        this.setState({
-            value: this.state.value + this.props.changeBy
-        });
+        // this.setState({
+        //     value: this.state.value + this.props.changeBy
+        // });
+        const newVal = this.props.value + this.props.changeBy;
+        this.props.clickHandler(this.props.index, newVal);
     }
     _subValue= () => {
-        this.setState({
-            value: this.state.value - this.props.changeBy
-        });
+        // this.setState({
+        //     value: this.state.value - this.props.changeBy
+        // });
+        const newVal = this.props.value - this.props.changeBy;
+        this.props.clickHandler(this.props.index, newVal);
     }
     _setValue= () => {
-        this.setState({
-            value: this.props.initialValue
-        });
+        // this.setState({
+        //     value: this.props.initialValue
+        // });
+        this.props.clickHandler(this.props.index, this.state.initialValue);
     }
 }
 
