571fb0ccdd3e4d816f2da0eb08d4acb59f34a7ae
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-03 11:54:17 -0500

    move renders to arrow functions

--- src/Counter.js
+++ src/Counter.js
@@ -15,25 +15,29 @@ class Counter extends React.Component {
         return(
             <div>
                 <h1>{this.state.value}</h1>
-                <button  onClick={() => {
-                    this.setState({
-                        value: this.state.value + this.props.changeBy
-                    });
-                }}>+</button>
-                <button  onClick={() => {
-                    this.setState({
-                        value: this.state.value - this.props.changeBy
-                    });
-                }}>-</button>
-                <button  onClick={() => {
-                    this.setState({
-                        value: this.props.initialValue
-                    });
-                }}>*</button>
+                <button  onClick={this._incrementValue}>+</button>
+                <button  onClick={this._subValue}>-</button>
+                <button  onClick={this._setValue}>*</button>
             </div>
         );
     }
     
+// write helper function as arrow func. they retain the correct value of the THIS keyword
+    _incrementValue= () => {
+        this.setState({
+            value: this.state.value + this.props.changeBy
+        });
+    }
+    _subValue= () => {
+        this.setState({
+            value: this.state.value - this.props.changeBy
+        });
+    }
+    _setValue= () => {
+        this.setState({
+            value: this.props.initialValue
+        });
+    }
 }
 
 
